ef89d511bfb9ead8c72d6fa013fa6d3b83853af0
brvasquezf <brvasquezf@gmail.com>
2023-11-06 20:56:37 -0300

    subida de proyecto

--- src/Components/Navigations.jsx
+++ src/Components/Navigations.jsx
@@ -3,20 +3,6 @@ import { Link } from 'react-router-dom'
 
 const Navigations = () => {
   return (
-    // <Navbar bg='dark' variant='dark' expand='lg'>
-    //     <Navbar.Toggle aria-controls='basic-navbar-nav' /> 
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //     <Nav className="me-auto">
-    //       <Nav.Link as={Link} to="/home" className="text-white">
-    //         Home
-    //       </Nav.Link>
-    //       <Nav.Link as={Link} to="/contact" className="text-white">
-    //         Contact
-    //       </Nav.Link>
-    //     </Nav>
-    //   </Navbar.Collapse>
-        
-    // </Navbar>
     <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Nav className="me-auto">
